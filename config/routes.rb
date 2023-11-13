61a65cd25502e2dd20b07cf500c7e283038adf0e
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-11-13 13:30:27 +0100

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "posts#index"
 end
